e20fd8cb307004f342b6bc709916439b733f1b32
kwakuoowusu <kwakuoowusu@gmail.com>
2016-03-10 19:17:34 -0500

    add parse pod

--- Fudge/AppDelegate.swift
+++ Fudge/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.initializeWithConfiguration(
+            ParseClientConfiguration(block: { (configuration:ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Fudge"
+                configuration.clientKey = "ojasdofjnbasiljgh9erugobljrnghibanew;eabguofhgluahgilanguahg"
+                configuration.server = "https://tranquil-ocean-79975.herokuapp.com/parse"
+            })
+        )
         return true
     }
 
