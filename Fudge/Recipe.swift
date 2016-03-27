1b67683351181ccc6e615cf17d45af85a5e350aa
Matt Del Signore <mattdelsig@me.com>
2016-03-27 16:42:51 -0400

    Started writing code for the Recipe model

--- Fudge/Recipe.swift
+++ Fudge/Recipe.swift
@@ -7,7 +7,44 @@
 //
 
 import UIKit
+import Parse
 
 class Recipe: NSObject {
 
+    var recipeId: Int?
+    var picUrl: NSURL?
+    var description: String?
+    var steps: [String]? //the steps you need to take to make the recipe
+    var ingredients: [String]?
+    var creator: PFUser? //the user that made the recipe
+    var url: NSURL? //the url for an externally hosted recipe
+    
+    //initalize the model from the pfobject
+    init(obj: PFObject) {
+        recipeId = obj["RecipeID"] as? Int
+        //check to see if the picture url exists as a valid string
+        let picString = obj["Picture"] as? String
+        if let picString = picString {
+             picUrl = NSURL(string: picString)
+        }else{
+            //otherwise make a blank url
+            picUrl = NSURL()
+        }
+        
+        description = obj["Description"] as? String
+        //TODO: Make sure we can cast the array like this
+        steps = obj["Steps"] as? [String]
+        ingredients = obj["Ingredients"] as? [String]
+        creator = obj["Creator"] as? PFUser
+        
+        //check for the external url, if it doesn't exist make it an empty string
+        let urlString = obj["Url"] as? String
+        if let urlString = urlString{
+            url = NSURL(string: urlString)
+        }else{
+            url = NSURL(string: "")
+        }
+    }
+    
+    //TODO: add method to make recips from an array, and post recipe to the server
 }
