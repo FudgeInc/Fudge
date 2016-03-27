7faf601f1edc99d7093613e4ada5fe344fb275f9
Matt Del Signore <mattdelsig@me.com>
2016-03-27 15:09:32 -0400

    Added Data models

---
+++ Fudge/Collection.swift
@@ -0,0 +1,13 @@
+//
+//  Collection.swift
+//  Fudge
+//
+//  Created by Matt on 3/27/16.
+//  Copyright © 2016 fudgeInc. All rights reserved.
+//
+
+import UIKit
+
+class Collection: NSObject {
+
+}
---
+++ Fudge/Recipe.swift
@@ -0,0 +1,13 @@
+//
+//  Recipe.swift
+//  Fudge
+//
+//  Created by Matt on 3/27/16.
+//  Copyright © 2016 fudgeInc. All rights reserved.
+//
+
+import UIKit
+
+class Recipe: NSObject {
+
+}
